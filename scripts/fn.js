652e9abae6a40b4dbcf104df0c99a6e9f20cabd6
DanilGrebnev <93824242+DanilGrebnev@users.noreply.github.com>
2022-08-11 16:21:41 +0300

    Add files via upload

---
+++ scripts/fn.js
@@ -0,0 +1,65 @@
+
+//Получаем случайное число
+function getRandom(min = 1, max = 9) {
+    // случайное число от min до (max+1)
+    const rand = min + Math.random() * (max + 1 - min)
+    return Math.floor(rand)
+}
+//Генерация и таблицы
+const setTable = (table, n = 10) => {
+    for (let i = 1; i < n; i++) {
+        let s = ''
+        for (let k = 1; k < n; k++) {
+            s += `<td data-x="${k}" data-y="${i}"></td>`
+        }
+        table.innerHTML += s
+    }
+}
+
+const $table = document.querySelector('table tbody')
+setTable($table)
+
+//генерация объекта со случайными координатами лис
+const getRandomFox = (n = 5) => {
+    let arr = []
+    for (let i = 0; i < n; i++) {
+        let rand = { x: getRandom(), y: getRandom() }
+        arr.forEach(el => {
+            if (el.x === rand.x && el.y === rand.y) {
+                arr = arr.filter(elem => elem !== el)
+                arr.push({ x: getRandom(), y: getRandom() })
+            }
+        })
+        arr.push(rand)
+    }
+    return arr
+}
+
+let a = getRandomFox(5)
+
+//Проверка на попадание в лису
+function hitInFox(x, y, e) {
+    const check = el => {
+        if (+el.x === x && +el.y === y) {
+            return true
+        }
+    }
+    if (a.some(check)) {
+        e.target.style = 'none'
+        e.target.classList.add('fox')
+        return true
+    }
+}
+
+//Можно будет выводить подсказку и местонахождении лис
+function help(arr) {
+    arr.forEach(td => {
+        a.forEach(fox => {
+            if (+td.dataset.x === +fox.x && +td.dataset.y === +fox.y) {
+                td.style.background = 'blue'
+            }
+        })
+    })
+}
+
+export { getRandom, setTable, getRandomFox, hitInFox, help, a, $table }
---
+++ scripts/index.js
@@ -0,0 +1,72 @@
+import { hitInFox, a, help, setTable, $table } from './fn.js'
+
+window.addEventListener('DOMContentLoaded', main)
+
+function main() {
+    document.addEventListener('click', getCoord)
+    let steps = 0
+    let count = 0
+    let $allTd = document.querySelectorAll('td')
+    let $count = document.querySelector('#fox-count')
+    const $footer = document.querySelector('#footer')
+
+    //Получение координат при нажатии
+    function getCoord(e) {
+        console.clear()
+        let fox = 0
+
+        if (e.target.localName !== 'td') return
+
+        if (steps === 45) {
+            alert('Хватит бездумно нажимать на ячейки! Включите логику!')
+            restart()
+        }
+        steps++
+        console.log(steps)
+
+        const x = +e.target.dataset.x
+        const y = +e.target.dataset.y
+
+        //При попадании точно в лису
+        if (hitInFox(x, y, e)) {
+            let div = document.createElement('div')
+            div.classList.add('fox')
+            $footer.prepend(div)
+            count++
+            $count.innerText = count
+
+            if (count === a.length) {
+                setTimeout(() => {
+                    alert('Поздравляю! Вы нашли всех Лис!')
+                }, 300)
+                restart()
+            } return
+        }
+        //Условие поиска лис
+        a.forEach(el => {
+            //По горизонтали и вертикали
+            if ((+el.x === x) || (+el.y === y)) fox++
+            //По диагонали
+            if (Math.abs(+el.x - x) === Math.abs(+el.y - y)) fox++
+        })
+        e.target.classList.add('show')
+        e.target.innerText = fox
+
+    }
+
+    //Можно будет выводить подсказку и местонахождении лис
+    // help($allTd)
+
+    function restart() {
+        $footer.innerHTML = ''
+        $table.innerHTML = ''
+        $footer.innerHTML = '<div id="fox-count"></div>'
+        $count = document.querySelector('#fox-count')
+        count = 0
+        setTable($table)
+        $allTd = document.querySelectorAll('td')
+    }
+
+}
+
+
